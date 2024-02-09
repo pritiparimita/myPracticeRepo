a8b7478c53f8f49275a289a4cb73d6a661e93031
pritiparimita <oec.deepak@gmail.com>
2024-02-09 21:01:10 +0530

    Commit for backend-1

--- TerraformPractice/main.tf
+++ TerraformPractice/main.tf
@@ -1,7 +1,20 @@
-resource "aws_instance" "ec2_mumbai" {
-  ami           = "ami-03f4878755434977f"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "terra_instance"
+resource "aws_s3_bucket" "s3-backend" {
+  bucket = "s3-backend-terraform-project-terra-project"
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3-backend-sse" {
+  bucket = aws_s3_bucket.s3-backend.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
   }
 }
+resource "aws_s3_bucket_versioning" "s3-backend-sse-versioning" {
+  bucket = aws_s3_bucket.s3-backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+   depends_on = [ aws_s3_bucket_server_side_encryption_configuration.s3-backend-sse ]
+}
+
+
